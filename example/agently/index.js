7c5df66e643afbee55928206d57a81d4495d458a
samuel <ferraxi@gmail.com>
2023-11-11 15:43:49 +0100

    agently

---
+++ example/agently/index.js
@@ -0,0 +1,39 @@
+const config = require('./config')
+
+class Prompt extends require('./prompt'){
+	constructor(){
+		super(config.prompt)
+
+	}
+}
+
+class DeskSession{
+	constructor(){
+	}
+
+	open(socket){
+		socket.on('text', data => prompt.text(data))
+		socket.on('offer', data => prompt.offer(data))
+		socket.on('answer', data => prompt.answer())
+		socket.on('ice', data => prompt.ice())
+	}
+
+	close(){
+		prompt.close()
+	}
+
+}
+
+class Desk extends require('./desk'){
+	constructor(){
+		super(config.desk)
+	}
+
+	createSession(socket){
+		return session
+	}
+}
+
+let session = new DeskSession()
+let desk = new Desk()
+let prompt = new Prompt()
---
+++ example/agently/prompt.js
@@ -0,0 +1,91 @@
+const msg = require('./message')
+
+class Prompt extends require('./session'){
+	constructor(config){
+		super()
+
+		this.credentials = config.credentials
+		this.address = config.credentials.address
+		this.greeting = config.greeting
+		this.buddy = config.peers.buddy
+
+		this.connect(config.host)
+	}
+
+	close(){
+		this.signoff(this.credentials)
+	}
+
+	onCommand(data, valid, signal){
+		switch(data.subject){
+			case 'text' : this.onText(data); break;
+			case 'offer' : this.onOffer(data); break;
+			case 'answer' : this.onAnswer(data); break;
+			case 'ice' : this.onIce(data); break;
+		}
+	}
+
+	onError(data){
+		console.log('error', data)
+	}
+
+	onConnected(data){
+		console.log(this.greeting)
+		this.signin(this.credentials)
+	}
+
+	onGranted(data){
+		console.log('granted', data)
+	}
+
+	onDenied(data){
+		console.log('denied', data)
+	}
+
+	//custom event
+	onText(_){}
+	onOffer(_){}
+	onAnswer(_){}
+	onIce(_){}
+
+
+	//custom method
+	text(data){
+		this.send('data',msg.create(
+			this.address,
+			this.buddy,
+			'text',
+			data
+		))
+	}
+
+	offer(data){
+		this.send('data',msg.create(
+			this.address,
+			this.buddy,
+			'offer',
+			data
+		))
+	}
+
+	answer(data){
+		this.send('data',msg.create(
+			this.address,
+			this.buddy,
+			'answer',
+			data
+		))
+	}
+
+	ice(data){
+		this.send('data',msg.create(
+			this.address,
+			this.buddy,
+			'ice',
+			data
+		))
+	}
+
+}
+
+module.exports = Prompt
